b4e87dc002848c6c0db0759483cba471bb3ec147
Melisa Hot <melisa.hot.03@gmail.com>
2021-10-29 01:44:11 +0200

    change blog

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
+          <Route exact path="/React-try">
+            <Home />
+          </Route>
           <Route path="/create">
             <Create />
           </Route>
