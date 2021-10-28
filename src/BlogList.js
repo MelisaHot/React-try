a6528421bf0bd5877b0c9314ba93096ca42c4de8
Melisa Hot <melisa.hot.03@gmail.com>
2021-10-29 01:48:34 +0200

    change blog

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,7 +6,7 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
-                <div className="blog-prewiew" key={blog.id}>
+                <div className="blog-preview" key={blog.id}>
                     <Link to={`/blogs/${blog.id}`}>
                         <h2>{blog.title}</h2>
                         <p>Written by: { blog.author }</p>
